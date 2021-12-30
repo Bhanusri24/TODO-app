fddfe712638f0053f75284ba84f9918f1fb285e8
Bhanusri24 <bhanumoturi24@gmail.com>
2021-12-30 23:51:48 +0530

    haha

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState("");
+  const [todos, setTodos] = useState([]);
+  useEffect(() => { document.title="TODO app" }, [])
+  const changeHandler = e => {
+    setData(e.target.value);
+  }
+  const submitHandler = e => {
+    e.preventDefault();
+    const newtodos = [...todos, data];
+    setTodos(newtodos);
+    setData("");
+  }
+  const deleteHandler = (indexvalue)=>{
+     const newTodos=todos.filter((todo,index)=>index!==indexvalue);
+     setTodos(newTodos);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div>
+      <center>
+        <h2>ToDo management</h2><br />
+        <form onSubmit={submitHandler}>
+          <input type="text" name="data" value={data} onChange={changeHandler} /> &nbsp; &nbsp;
+          <input type="submit" value="add" />
+        </form>
+        <div>{
+          todos.map((value, index) => <div key={index}>{value}&nbsp;<button onClick={()=>deleteHandler(index)}>delete</button></div>)
+        }
+        </div>
+      </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
